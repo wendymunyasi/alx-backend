dfc8ef3c9b942ebb699147d1d277d434e7e25ae7
wendy munyasi <wendymunyasi@gmail.com>
2023-01-21 19:24:29 +0300

    Add task 2 - Implement a get_hyper method

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python3
+"""Module for task 1
+"""
+import csv
+from typing import List
+
+
+def index_range(page, page_size):
+    """Returns a tuple of size two containing a start index and an end
+    index corresponding to the range of indexes to return in a list for the
+    given pagination parameters.
+
+    Args:
+        page (int): the number of the page
+        page_size (int): the number of items per page
+
+    Returns:
+        tuple: start and end indexes of current page.
+    """
+    # calculate start index by subtracting 1 from the current page number
+    # and then multiplying by the page size
+    start_index = (page - 1) * page_size
+    # calculate end index by adding the start index to the page size
+    end_index = start_index + page_size
+    # return the start and end indexes as a tuple
+    return (start_index, end_index)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Retrieves a specific page of the dataset of popular baby names.
+
+        Args:
+            page (int, optional): the number of the page to retrieve.
+            Defaults to 1.
+            page_size (int, optional): the number of items per page.
+            Defaults to 10.
+
+        Returns:
+            List[List]: a list of rows representing the requested page of
+            the dataset.
+        """
+        # Verify that both page and page_size are integers greater than 0
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+        # Find the correct start and end indexes for the current page
+        start_index, end_index = index_range(page, page_size)
+        # Retrieve the dataset
+        dataset = self.dataset()
+        # Check if the end_index is greater than the length of the dataset
+        # If it is, return an empty list
+        if end_index > len(dataset):
+            return []
+        # Return the appropriate page of the dataset
+        return dataset[start_index:end_index]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10):
+        """Retrieves a specific page of the dataset of popular baby names
+        along with pagination metadata.
+
+        Args:
+            page (int, optional): The number of the page to retrieve. Defaults
+            to 1.
+            page_size (int, optional): The number of items per page. Defaults
+            to 10.
+
+        Returns:
+            dict: A dictionary containing the following key-value pairs:
+                page_size (int): the length of the returned dataset page.
+                page (int): the current page number.
+                data (List[List]): the dataset page.
+                next_page (int or None): number of the next page, None if no
+                next page
+                prev_page (int or None): number of the previous page, None if
+                no previous page
+                total_pages (int): the total number of pages in the dataset as
+                an integer
+
+        Raises:
+            AssertionError: If either of the input arguments is not an integer
+            greater than 0.
+        """
+        # get the data by calling the get_page method
+        data = self.get_page(page, page_size)
+        # calculate the page size
+        page_size = len(data)
+        # calculate the total pages
+        var_1 = page_size + (len(self.dataset()) % page_size != 0)
+        total_pages = len(self.dataset()) // var_1
+        # check if there is a next page
+        next_page = page + 1 if page_size * page < len(self.dataset()) \
+            else None
+        # check if there is a previous page
+        prev_page = page - 1 if page > 1 else None
+        # return the dictionary containing the pagination data
+        return {
+            'page_size': page_size,
+            'page': page,
+            'data': data,
+            'next_page': next_page,
+            'prev_page': prev_page,
+            'total_pages': total_pages
+        }
