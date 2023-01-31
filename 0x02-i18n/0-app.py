566f53f19bf5a0f66ff28e13278be72caef33d06
wendy munyasi <wendymunyasi@gmail.com>
2023-01-31 14:01:25 +0300

    Add task 0 - basic flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Module for task 0
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def index():
+    """The index function displays the home page of the web application.
+
+    Returns:
+        str: contents of the home page.
+    """
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run()
