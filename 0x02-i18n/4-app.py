bc5e40b3b283d484c13a582a4b80dec25d3d99a5
wendy munyasi <wendymunyasi@gmail.com>
2023-01-31 16:24:16 +0300

    Add task 4 - Force locale with URL parameter

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+"""Module for task 4
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel, localeselector
+
+app = Flask(__name__)
+
+app.url_map.strict_slashes = False
+
+
+class Config:
+    """Represents a Flask Babel configuration.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route("/")
+def index_3():
+    """The index function displays the home page of the web application.
+
+    Returns:
+        str: contents of the home page.
+    """
+    return render_template("3-index.html")
+
+
+@babel.localeselector
+def get_locale():
+    """etermines the best match for the client's preferred language.
+
+    This function uses Flask's request object to access the client's preferred
+    languages and the app's supported languages (defined in the Config class)
+    to determine the best match. The best match is then returned as the locale.
+
+    Returns:
+        str: The locale code for the best match (e.g. "en", "fr").
+    """
+    # Get the locale parameter from the incoming request
+    locale = request.args.get('locale')
+    # Get list of supported languages from Config
+    supported_languages = app.config["LANGUAGES"]
+    if locale and locale in supported_languages:
+        # If the locale parameter is present and is a supported locale,
+        # return it
+        return locale
+    else:
+        # Use request.accept_languages to get the best match
+        best_match = request.accept_languages.best_match(supported_languages)
+        return best_match or 'en'
+
+
+if __name__ == "__main__":
+    app.run()
