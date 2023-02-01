073415707378f48c1eded6d71f44220c60470c5d
wendy munyasi <wendymunyasi@gmail.com>
2023-02-01 23:49:49 +0300

    Fix pycodestyle issues

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -67,6 +67,7 @@ def get_locale() -> str:
         return header_locale
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
+
 def get_user() -> Union[Dict, None]:
     """Returns a user dictionary based on the given ID
 
