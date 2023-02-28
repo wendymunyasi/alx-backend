df737209a0898a4faab02f28da12f7ca1b3a835c
wendy munyasi <wendymunyasi@gmail.com>
2023-02-28 14:03:22 +0300

    Add task 5 - Node Redis client publisher and subscriber

---
+++ 0x03-queuing_system_in_js/5-publisher.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+// Write a function named publishMessage:
+//  - It will take 2 arguments: message (string), and time (integer - in ms)
+//  - After time millisecond:
+//    - The function should log to the console About to send MESSAGE
+//    - The function should publish to the channel holberton school channel,
+//      the message passed in argument after the time passed in arguments
+// At the end of the file, call:
+//  - publishMessage("Holberton Student #1 starts course", 100);
+//  - publishMessage("Holberton Student #2 starts course", 200);
+//  - publishMessage("KILL_SERVER", 300);
+//  - publishMessage("Holberton Student #3 starts course", 400);
+import redis from 'redis';
+
+// Create Redis client
+const publisher = redis.createClient();
+
+// On connect
+publisher.on('connect', function() {
+  console.log('Redis client connected to the server');
+});
+
+// On error
+publisher.on('error', function(error) {
+  console.error(`Redis client not connected to the server: ${error}`);
+});
+
+// Function to publish message after a certain time
+const publishMessage = (message, time) => {
+  // setTimeout(function() { WORKS TOO
+  setTimeout(() => {
+    console.log(`About to send ${message}`);
+    publisher.publish('holberton school channel', message);
+  }, time);
+}
+
+// Publish messages
+publishMessage('Holberton Student #1 starts course', 100);
+publishMessage('Holberton Student #2 starts course', 200);
+publishMessage('KILL_SERVER', 300);
+publishMessage('Holberton Student #3 starts course', 400);
---
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env noded to the server: ERROR MESSAGE
+// It should subscribe to the channel holberton school channel
+// When it receives message on the channel holberton school channel,
+// it should log the message to the console
+// When the message is KILL_SERVER, it should unsubscribe and quit
+import redis from 'redis';
+
+// Create Redis client
+const subscriber = redis.createClient();
+
+// On connect
+// subscriber.on('connect', function() { WORKS TOO
+subscriber.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+// On error
+// subscriber.on('error', function(error) { WORKS TOO
+  subscriber.on('error', (error) => { 
+  console.error(`Redis client not connected to the server: ${error}`);
+});
+
+// Subscribe to holberton school channel
+subscriber.subscribe('holberton school channel');
+
+// On message received
+// subscriber.on('message', function(channel, message) { WORKS TOO
+  subscriber.on('message', (channel, message) => {
+  console.log(`${message}`);
+  if (message === 'KILL_SERVER') {
+    subscriber.unsubscribe();
+    subscriber.quit();
+  }
+});
