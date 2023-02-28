103552347b399f7f2203d764ce466260c97de842
wendy munyasi <wendymunyasi@gmail.com>
2023-02-28 13:01:44 +0300

    Add task 1 - script that connects to redis server

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+// script that logs to the console "Redis client connected to the server"
+//  when the connection to Redis works correctly
+// It logs to the console "Redis client not connected to the server: ERROR_MESSAGE"
+// when the connection to Redis does not work
+import redis from 'redis';
+
+const client = redis.createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (error) => {
+  console.log(`Redis client not connected to the server: ${error.message}`);
+});
